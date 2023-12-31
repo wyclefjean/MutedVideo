19b81a6df909166b1ad5d726689ebd7da7fc3caa
wyclefjean <155137336+wyclefjean@users.noreply.github.com>
2023-12-31 03:29:56 -0800

    main
    m

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-document.getElementById('muteForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+function addWord() {
+    var word = document.getElementById("customWord").value;
+    var checkboxes = document.getElementById("wordCheckboxes");
+    checkboxes.innerHTML += '<input type="checkbox" id="' + word + '" name="words" value="' + word + '"><label for="' + word + '">' + word + '</label><br>';
+}
 
-    let formData = new FormData();
-    let selectedWords = Array.from(document.getElementById('words').selectedOptions).map(option => option.value);
-    let customWord = document.getElementById('customWord').value;
-    if (customWord) {
-        selectedWords.push(customWord);
-    }
-
-    let videoFile = document.getElementById('videoUpload').files[0];
-    if (!videoFile) {
-        alert('Please upload a video.');
-        return;
-    }
-
-    formData.append('video', videoFile);
-    formData.append('words', selectedWords.join(','));
-
-    fetch('/mute-video', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('responseMessage').innerText = data;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-});
+function processVideo() {
+    document.getElementById("loading").style.display = "block";
+    // Implement the video processing here
+}
